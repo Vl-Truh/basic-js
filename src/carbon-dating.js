7f9edf7fb39a5978aa42e49ef6f70ff036b921f6
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-03-02 19:09:40 +0300

    fix: flow control modify

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,13 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY = 15; 
-const C = 5730;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if ((typeof(sampleActivity) !== 'string' || 'NaN') ||
-      sampleActivity > MODERN_ACTIVITY ||
-      sampleActivity > HALF_LIFE_PERIOD ||
-      sampleActivity <= 0) return false;
+  if ((typeof(sampleActivity) !== 'string') ||
+      Number.parseFloat(sampleActivity) > MODERN_ACTIVITY ||
+      Number.parseFloat(sampleActivity) <= 0) return false;
       
   sampleActivity = Number.parseFloat(sampleActivity);
   
