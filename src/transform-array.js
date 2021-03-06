cd6420e7d958e6b08204f4f2c297fbae3d06af75
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-03-06 13:07:43 +0300

    fix: control flow modify

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,18 +9,26 @@ module.exports = function transform(arr) {
 	let mArr = [...arr];
   
   for (let i = 0; i < mArr.length; i++) {
-  	if (mArr[i] == '--discard-next') {
+    if ((mArr[i] == '--discard-next') && i + 1 <= mArr.length - 1) {
     	mArr.splice(i, 2);
+    }
+	  
+    else if ((mArr[i] == '--discard-next') && i + 1 > mArr.length - 1) {
+    	mArr.splice(i, 1);
     }
     
     else if (mArr[i] == '--discard-prev') {
     	mArr.splice(i - 1, 2);
     }
     
-    else if (mArr[i] == '--double-next') {
+    else if ((mArr[i] == '--double-next') && i + 1 <= mArr.length - 1) {
     	mArr[i] = mArr[i + 1];
     }
-    
+   
+    else if ((mArr[i] == '--double-next') && i + 1 > mArr.length - 1) {
+    	mArr.splice(i, 1);
+    }
+	  
     else if (mArr[i] == '--double-prev') {
     	mArr[i] = mArr[i - 1]
     }
