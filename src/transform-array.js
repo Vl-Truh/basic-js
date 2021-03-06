6d0d0f42f7d91d2d470cc4464833789a62764b43
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-03-06 12:54:16 +0300

    feat: solution add

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+	if (!Array.isArray(arr)) {
+  	throw new Error();
+  }
+
+	let mArr = [...arr];
+  
+  for (let i = 0; i < mArr.length; i++) {
+  	if (mArr[i] == '--discard-next') {
+    	mArr.splice(i, 2);
+    }
+    
+    else if (mArr[i] == '--discard-prev') {
+    	mArr.splice(i - 1, 2);
+    }
+    
+    else if (mArr[i] == '--double-next') {
+    	mArr[i] = mArr[i + 1];
+    }
+    
+    else if (mArr[i] == '--double-prev') {
+    	mArr[i] = mArr[i - 1]
+    }
+  }
+  
+  return mArr;
+}
+
